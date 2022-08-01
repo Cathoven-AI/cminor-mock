cdbbb2c157be74fea8e05342f2acb1495d22f060
Erdi Tac <erditac@gmail.com>
2022-08-01 10:26:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt', 'r') as f:
     requirements = f.read().strip().split('\n')
 
 setup(
-    name='solar',
+    name='solarcat',
     version='1.5.2',
     description='SOLAR: System of LAnguage Retention',
     long_description=long_description,
