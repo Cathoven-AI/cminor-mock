c3284653676bffa813dc08d4483da126cb5d3405
lngmngx <lorenzolmx@gmail.com>
2023-03-22 10:15:40 +0800

    Improved retry and promts
    Signed-off-by: lngmngx <lorenzolmx@gmail.com>

--- generator.py
+++ generator.py
@@ -4,7 +4,9 @@ class AdoQuestionGenerator(object):
     def __init__(self, openai_api_key=None):
         self.openai_api_key = openai_api_key
 
-    def generate_questions(self, text, n=10, kind='multiple_choice', auto_retry=True, override_messages=None):
+    def generate_questions(self, text, n=10, kind='multiple_choice', auto_retry=3, override_messages=None):
+
+        auto_retry = min(int(auto_retry),3)
 
         if self.openai_api_key is None:
             warnings.warn("OpenAI API key is not set. Please assign one to .openai_api_key before calling.")
@@ -14,15 +16,15 @@ class AdoQuestionGenerator(object):
 
         n_why = n//2
         json_format = '''[{"question": "Why is this the case?","choices": ["Some choice","Some choice","Some choice","Some choice"],"answer_index": 0},{"question": "What is this?","choices": ["Some choice","Some choice","Some choice","Some choice"],"answer_index": 2}]'''
-        content = f'''Generate {n} multiple choice questions for this text. {n-n_why} of them are questions about main ideas and details. The other {n_why} are "why" questions. Each question has four choices. The answers must be logically correct and the other choices must be incorrect. Return the results as "question", "choices", and "answer_index" as a Python dictionary that can be parsed by ast.literal_eval like this: {json_format}. The answer_index ranges from 0 to 3.
-
+        content = f'''Generate {n} multiple choice questions for this text. {n-n_why} of the questions are about main ideas and details. The other {n_why} are "why" questions. Each question has four choices. The answers must be logically correct and the other choices must be incorrect. You MUST arrange the questions as a Python list of dictionaries like this: {json_format}. Each dictionary is one question with keys "question", "choices", and "answer_index". The answer_index ranges from 0 to 3. I will need to parse the list directly using ast.literal_eval in Python.
         Text: {text}'''
 
+        messages = [{"role": "system", "content": '''You are an English teacher who creates reading comprehension questions for an exam in Python code.'''},{"role": "user", "content": content}]
         if override_messages is None:
             try:
                 completion = openai.ChatCompletion.create(
                     model="gpt-3.5-turbo",
-                    messages=[{"role": "system", "content": '''You are an English teacher creating reading comprehension questions for an exam.'''},{"role": "user", "content": content}]
+                    messages=messages
                 )
             except Exception as e:
                 return {'error':e.__class__.__name__,'detail':str(e)}
@@ -34,21 +36,30 @@ class AdoQuestionGenerator(object):
                 )
             except Exception as e:
                 return {'error':e.__class__.__name__,'detail':str(e)}
-        print(completion)
+            
         response = completion['choices'][0]['message']['content'].strip()
+        questions = self.parse_questions(response)
+
+        if questions is None:
+            if auto_retry>0:
+                if auto_retry%2==1:
+                    return self.generate_questions(text, n=n, kind=kind, auto_retry=auto_retry-1, override_messages=messages+[{"role": completion['choices'][0]['message']['role'], "content": completion['choices'][0]['message']['content']},
+                                                                                                              {"role": "user", "content": f"The questions you returned are not in Python dictionary format. Return them in as a Python list of dictionaries like this example: {json_format}"}])
+                else:
+                    return self.generate_questions(text, n=n, kind=kind, auto_retry=auto_retry-1)
+            else:
+                return {'error':"SyntaxError",'detail':f"The bot didn't return the questions in Python dictionary format. response: {response}"}
+
+        return questions
+    
+    def parse_questions(self, response):
         try:
-            response = response[response.index('['):]
-            questions = json.loads(response)
+            response = response[response.index('['):response.rfind(']')+1]
+            questions = ast.literal_eval(response)
         except:
             try:
-                response = response[response.index('['):]
-                questions = ast.literal_eval(response)
-            except Exception as e:
-                if auto_retry:
-                    return self.generate_questions(text, n=n, kind=kind, auto_retry=False, override_messages=[{"role": "system", "content": '''You are an English teacher creating reading comprehension questions for an exam.'''},
-                                                                                                              {"role": "user", "content": content},
-                                                                                                              {"role": completion['choices'][0]['message']['role'], "content": completion['choices'][0]['message']['content']},
-                                                                                                              {"role": "user", "content": f"The questions you returned are not in Python dictionary format. Return them in Python dictionary format like this example: {json_format}"}])
-                else:
-                    return {'error':e.__class__.__name__,'detail':str(e)+" The bot didn't return the questions in Python dictionary format."}
+                response = response[response.index('['):response.rfind(']')+1]
+                questions = json.loads(response)
+            except:
+                return None
         return questions
