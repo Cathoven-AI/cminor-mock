849fd38935676316638d1c5b6be14699bb865733
lngmngx <theliangmx@gmail.com>
2023-12-21 12:44:38 +0800

    Strip - from lemmas
    Signed-off-by: lngmngx <theliangmx@gmail.com>

--- lemmatizers.py
+++ lemmatizers.py
@@ -97,6 +97,9 @@ def fine_lemmatize(x,doc,spacy):
     elif x.pos_ == "PUNCT" and len(re.findall(r'[A-Za-z]', x.orth_))>=6:
         x.pos_ = "ADJ"
 
+    no_dash = x.lemma_.strip('-')
+    if no_dash!='':
+        x.lemma_ = no_dash
     #if x.lemma_.endswith('.') and not x.orth_[0].isupper() and not '.' in x.lemma_.strip('.'):
     #    x.lemma_ = x.lemma_.strip('.')
     return x
