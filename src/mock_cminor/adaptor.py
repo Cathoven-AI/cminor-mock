7b0ee3e80916286e71ab2f773548942fbdcd5de4
Erdi Tac <erditac@gmail.com>
2024-05-24 22:51:20 +0800

    add antropic to adaptor

--- src/mock_cminor/adaptor.py
+++ src/mock_cminor/adaptor.py
@@ -1,9 +1,10 @@
 from .get_results import get_adaptor
 
 class AdoLevelAdaptor:
-    def __init__(self,analyser, openai_api_key):
+    def __init__(self, analyser, openai_api_key=None, antropic_key=None):
         self.analyser = analyser
         self.openai_api_key = openai_api_key
+        self.antropic_key = antropic_key
         
     def adapt(self, text, target_level, target_adjustment=0.5, even=False, by="paragraph", min_piece_length=200, n=1, auto_retry=False, return_result=True, model="cefr"):
 
